95921884f984092b5af84221315e51ac5a4fb78f
Joe Dwyer <deeznodes@gmail.com>
2020-02-17 20:31:18 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Koggz App (Beta)`,
+    description: `Digitally Fabricated Parts On Demand.`,
     author: `@gatsbyjs`,
   },
   plugins: [
