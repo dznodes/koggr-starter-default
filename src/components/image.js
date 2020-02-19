1d8891979c7474262877f5edbf71e8279846494d
Joe Dwyer <deeznodes@gmail.com>
2020-02-19 12:29:51 -0500

    Update image.js
    added the home_screen demo image

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "koggz_qr.png" }) {
+      placeholderImage: file(relativePath: { eq: "Home_Mockup.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
