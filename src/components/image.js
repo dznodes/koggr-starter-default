6db6f9230d76298ba23a2dea5d7db99184afc730
Joe Dwyer <deeznodes@gmail.com>
2020-02-24 12:06:50 -0500

    Update image.js

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "Home_Mockup.png" }) {
+      placeholderImage: file(relativePath: { eq: "Home_Screen_Mockup.png" }) {
         childImageSharp {
           fluid(maxWidth: 1200) 
           padding: `1.45rem 1.0875rem`,{
