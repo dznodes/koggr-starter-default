221c52c57d287b1249cce03a823b6b4a81ad2048
Joe Dwyer <deeznodes@gmail.com>
2020-03-14 13:28:54 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>We make parts on demand.</h1>
-    <p>To try our mobile app please scan this QR code.</p>
+    <h1>We make custom parts on demand.</h1>
+    <p>Scan this QR code to try our mobile app.</p>
     <div style={{ maxWidth: `1294px`, marginBottom: `1.45rem`, padding: '20px' }}>
       <Image />
     </div>
